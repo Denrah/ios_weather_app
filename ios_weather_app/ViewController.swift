96ad7daf040fba317fbe0cefe6f0dc30d73c0c7e
Denis Sharapov <dsharapov@kode-t.ru>
2019-09-03 10:29:49 +0700

    Initial Commit

---
+++ ios_weather_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_weather_app
+//
+//  Created by iOS Intern on 03/09/2019.
+//  Copyright © 2019 iOS Intern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
