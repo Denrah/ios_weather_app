d5c599402ce31c322d304d2acc109a78e61e20cd
Denis Sharapov <dsharapov@kode-t.ru>
2019-09-03 14:11:26 +0700

    Add map screen, geocoding by tap, searcher to title and modal

---
+++ ios_weather_app/Scenes/Map/MapViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewModel.swift
+//  ios_weather_app
+//
+//  Created by iOS Intern on 03/09/2019.
+//  Copyright © 2019 iOS Intern. All rights reserved.
+//
+
+import Foundation
+
+class MapViewModel {
+    
+    weak var coordinatorDelegate: MapCoordinator?
+    
+    init() {
+        
+    }
+    
+}
