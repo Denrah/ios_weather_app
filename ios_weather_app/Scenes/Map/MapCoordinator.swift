ecfb5e6989032d574b14ab7f00330039c1f2e787
Denis Sharapov <dsharapov@kode-t.ru>
2019-09-03 10:46:32 +0700

    Add base coordinators

---
+++ ios_weather_app/Scenes/Map/MapCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  MapCoordinator.swift
+//  ios_weather_app
+//
+//  Created by iOS Intern on 03/09/2019.
+//  Copyright © 2019 iOS Intern. All rights reserved.
+//
+
+import UIKit
+
+class MapCoordinator: Coordinator {
+    
+    let rootViewController: UINavigationController
+    
+    init(rootViewController: UINavigationController) {
+        self.rootViewController = rootViewController
+    }
+    
+    override func start() {
+       
+    }
+}
